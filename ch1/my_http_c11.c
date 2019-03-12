a70cea196b0b4e19da3e18a8f1beb45661850d56
Tanveer Salim <tanveerasalim@gitlab.com>
2019-03-12 01:02:56 -0500

    master

---
+++ ch1/my_http_c11.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+
+#define MAX_BUFFER_SIZE 1024
+
+#define GET_REQUEST_LEN 32
+
+#define TEST_LEN ( ( (MAX_BUFFER_SIZE)*(2) )+ (GET_REQUEST_LEN) )
+
+void get_params(char const * uri,char ** hostname,char ** pathname)
+{
+	static char host[MAX_BUFFER_SIZE];
+
+	static char url[MAX_BUFFER_SIZE];
+
+	static char * path_slash = NULL;
+
+	strncat_s(url,MAX_BUFFER_SIZE,uri,MAX_BUFFER_SIZE-strlen(url)-1);
+
+
+	if ( strstr(url,"//") != NULL )
+	{ 
+		strncat_s(*hostname,MAX_BUFFER_SIZE,&url[abs(strstr(url,"//")-&url[0]) + 2],MAX_BUFFER_SIZE - strlen(*hostname)-1);
+
+
+		
+	}
+
+	else
+	{
+		strncat_s(*hostname,MAX_BUFFER_SIZE,&url[0],MAX_BUFFER_SIZE-strlen(*hostname)-1);
+
+		
+	}
+	
+
+	if (strstr(*hostname,"/") != NULL)	
+	{	
+		
+		strncat_s(*pathname,MAX_BUFFER_SIZE,&url[abs( strstr(*hostname,"/")  - &url[0] ) + 1],MAX_BUFFER_SIZE-strlen(*pathname)-1);
+	}
+
+	else
+	{
+		*pathname[0] = '\0';	
+			
+	}
+
+// separating hostname from pathname
+	
+	if ( strstr(*hostname,"/") != NULL )
+	{
+		*strstr(*hostname,"/") = '\0';
+	}
+
+}
+
+int main(int argc, char ** argv)
+{
+	if ( argc < 2)
+	{
+		fprintf(stderr,"%d: Missing URL",__LINE__);
+
+		exit(EXIT_FAILURE);
+	}	
+	
+	static struct addrinfo hints; static struct addrinfo *res = NULL; 
+	
+	static struct addrinfo *p = NULL; 
+
+	memset_s(&hints,sizeof(hints),0,sizeof(hints));
+
+	int sockd = 0;
+
+	static char msg[MAX_BUFFER_SIZE];
+
+	static char test[1024*2+32];
+
+	static char host[MAX_BUFFER_SIZE];
+
+	static char * host_p = &host[0];
+
+	static char path[MAX_BUFFER_SIZE];
+
+	static char * path_p = &path[0];
+
+	get_params(argv[1],&host_p,&path_p);
+
+	printf("Host:%s\nPath:%s\n",host,path);
+	
+	snprintf_s(test,TEST_LEN,"GET /%s HTTP/1.1\r\n\0",path);
+
+	int recv_bytes = 0, sent_bytes = 0, gstrerror = 0;
+
+	memset_s(&hints,sizeof(hints),0,sizeof(hints));
+
+	hints.ai_family = AF_UNSPEC;
+
+	hints.ai_socktype = SOCK_STREAM;
+	
+	if ( (gstrerror = getaddrinfo(host,"http",&hints,&res) ) != 0)
+	{
+		fprintf(stderr,"%d: getaddrinfo(): %s\n",__LINE__,gai_strerror(gstrerror));
+		exit(EXIT_FAILURE);
+
+	}
+
+	for ( p = res; p!= NULL; p = p->ai_next)
+	{
+		if ( (p->ai_family == AF_INET) && ((sockd = socket(p->ai_family,p->ai_socktype,p->ai_protocol)) != -1) )
+		{
+			if (connect(sockd,(struct sockaddr *)p->ai_addr,p->ai_addrlen) != -1)
+			{ res = p; break; }	
+			
+		}
+
+	}
+	
+	if ( p == NULL)
+	{
+		fprintf(stderr,"%d: ",__LINE__);
+		perror("socket()");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Data Sent and Received\n");
+	
+	if ( (sent_bytes = send(sockd,test,strlen(test),0)) == -1)
+	{
+		fprintf(stderr,"%d: ",__LINE__);
+
+		perror("send");
+		
+		exit(EXIT_FAILURE);
+	}
+	
+	snprintf_s(test,TEST_LEN,"Host: %s\r\n\0",host);
+
+	if ( (sent_bytes = send(sockd,test,strlen(test),0)) == -1)
+	{
+		fprintf(stderr,"%d: ",__LINE__);
+
+		perror("send");
+		
+		exit(EXIT_FAILURE);
+	}
+	
+	snprintf_s(test,TEST_LEN,"Connection: close\r\n\r\n\0");
+
+	if ( (sent_bytes = send(sockd,test,strlen(test),0)) == -1)
+	{
+		fprintf(stderr,"%d: ",__LINE__);
+
+		perror("send");
+		
+		exit(EXIT_FAILURE);
+	}
+	printf("%s",recv_bytes,msg);
+
+	while ( ( ( recv_bytes = recv(sockd,msg,sizeof(msg),0) )  > 0 ) 
+		&& (msg[0] != '\0') )
+	{
+		printf("%s",msg);
+		
+		memset_s(msg,MAX_BUFFER_SIZE,0,sizeof(msg));	
+	}
+
+
+	close(sockd);
+		
+	freeaddrinfo(res);
+	
+	return 0;
+}
